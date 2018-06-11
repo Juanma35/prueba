def6aa13b00c8f3b206c9dacd05ad6822554f029
S027515 <S027515@HUSEPTJUANMA.ssib.local>
2018-06-11 19:05:22 +0200

    primera version

---
+++ spring-boot-data-jpa/src/test/java/com/bolsadeideas/springboot/app/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,26 @@
+package com.bolsadeideas.springboot.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.bolsadeideas.springboot.app.controllers.ClienteController;
+import com.bolsadeideas.springboot.app.models.service.ClienteServiceImpl;
+import com.bolsadeideas.springboot.app.models.service.ProductoServiceImpl;
+
+import junit.framework.Assert;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class SpringBootDataJpaApplicationTests  {
+
+	@Test
+	public void contextLoads() {
+		
+	
+
+
+	}}
+
+
