28770a352d969aa1e2e6661addcd9f71e1fb2a00
juanma35 <tecnicohome@hotmail.es>
2018-06-12 08:40:19 +0200

    Revert "primera version"
    This reverts commit def6aa13b00c8f3b206c9dacd05ad6822554f029.

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/IFactura.java
+++
@@ -1,7 +0,0 @@
-package com.bolsadeideas.springboot.app.models.dao;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.bolsadeideas.springboot.app.models.entity.Factura;
-
-public interface IFactura extends PagingAndSortingRepository<Factura, Long>{}
--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ITecnicoDao.java
+++
@@ -1,9 +0,0 @@
-package com.bolsadeideas.springboot.app.models.dao;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.bolsadeideas.springboot.app.models.entity.Tecnico;
-
-public interface ITecnicoDao extends PagingAndSortingRepository<Tecnico, Long>{
-
-}
